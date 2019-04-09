a4639a68a5c09980f56b8a4156d8d3c012cdd07b
Samory Harris <samory070@gmail.com>
2019-04-09 16:07:33 -0400

    Update Weather.js with new dataset, error hanlding in App.js

--- src/App.js
+++ src/App.js
@@ -3,52 +3,77 @@ import './App.css';
 import Title from './Components/Title.js';
 import Form from './Components/Form.js';
 import Weather from './Components/Weather.js';
-
+// API key
 const API_KEY = '69e016510672bc434645f7b43f25c61a';
 
 
 class App extends Component {
     state = {
         temperature: undefined,
+        temp_min: undefined,
+        temp_max: undefined,
+        wind: undefined,
         city: undefined,
         country: undefined,
         humidity: undefined,
         description: undefined,
         error: undefined
     }
+    // Get values from API
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
 
-        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        // API data converted to json using dot notation
         const data = await api_call.json();
-
         console.log(data);
-        this.setState({
-            temperature: data.main.temp,
-            city: data.sys.country,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-            error: ''
-        });
+        if(city && country) {
+            // Sets new state of undefined state
+            this.setState({
+                temperature: data.main.temp,
+                temp_min: data.main.temp_min,
+                temp_max: data.main.temp_max,
+                wind: data.wind.speed,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ''
+            });
+
+        }else {
+            this.setState({
+                temperature: undefined,
+                temp_min: undefined,
+                temp_max: undefined,
+                wind: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter values."
+            });
+        }
+        // console.log(this.state)
     }
     render() {
         return (
             <div>
                 < Title />
-            < Form getWeather={this.getWeather}/>
-        < Weather
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-
-        />
+                < Form getWeather={this.getWeather} />
+                < Weather
+                    temperature={this.state.temperature}
+                    temp_min={this.state.temp_min}
+                    temp_max={this.state.temp_max}
+                    wind={this.state.wind}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                />
             </div>
         );
     }
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -4,11 +4,14 @@ class Weather extends React.Component {
     render() {
         return(
             <div>
-                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
-                {this.props.humidity && <p>Humidity: {this.props.humidty}</p>}
-                {this.props.error && <p>Error: {this.props.error}</p>}
+                { this.props.city && this.props.country &&  <p>Location: { this.props.city }, { this.props.country }</p>}
+                { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+                { this.props.description && <p>Conditions: { this.props.description }</p> }
+                { this.props.temp_min && <p>Low Temp: { this.props.temp_min }</p> }
+                { this.props.temp_max && <p>High Temp: { this.props.temp_max }</p> }
+                { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
+                { this.props.wind && <p>Wind Speed: { this.props.wind }</p> }
+                { this.props.error && <p>{ this.props.error }</p> }
             </div>
         );
     }
