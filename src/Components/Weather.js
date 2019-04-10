1be154dcd64b112cdbf528a331193ed19f13f674
Samory Harris <samory070@gmail.com>
2019-04-10 11:53:01 -0400

    Add classNames in Weather.js

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -4,15 +4,47 @@ import React, { Component } from 'react';
 
 // stateless component
 const Weather = props => (
-        <div className="waether__info">
-            { props.city && props.country &&  <p>Location: { props.city }, { props.country }</p>}
-            { props.temperature && <p>Temperature: { props.temperature }</p> }
-            { props.description && <p>Conditions: { props.description }</p> }
-            { props.temp_min && <p>Low Temp: { props.temp_min }</p> }
-            { props.temp_max && <p>High Temp: { props.temp_max }</p> }
-            { props.humidity && <p>Humidity: { props.humidity }</p> }
-            { props.wind && <p>Wind Speed: { props.wind }</p> }
-            { props.error && <p>{ props.error }</p> }
+        <div className="weather__info">
+            {
+                props.city && props.country &&  <p className="weather__key">Location:
+                    <span className="weather__value"> { props.city }, { props.country }</span>
+                </p>
+            }
+            {
+                props.temperature && <p className="weather__key">Temperature:
+                    <span className="weather__value" > { props.temperature }</span>
+                </p>
+            }
+            {
+                props.description && <p className="weather__key">Conditions:
+                    <span className="weather__value"> { props.description }</span>
+                </p>
+            }
+            {
+                props.temp_min && <p className="weather__key">Low Temp:
+                    <span className="weather__value">{ props.temp_min }</span>
+                </p>
+            }
+            {
+                props.temp_max && <p className="weather__key">High Temp:
+                    <span className="weather__value"> { props.temp_max }</span>
+                </p>
+            }
+            {
+                props.humidity && <p className="weather__key">Humidity:
+                    <span className="weather__value"> { props.humidity }</span>
+                </p>
+            }
+            {
+                props.wind && <p className="weather__key">Wind Speed:
+                    <span className="weather__value"> { props.wind }</span>
+                </p>
+            }
+            {
+                props.error && <p className="weather__error">
+                    { props.error }
+                </p>
+            }
         </div>
 
 )
